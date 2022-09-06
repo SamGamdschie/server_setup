c3e5fca1a7c9fe208e8697561ca7e8476e318e19
Thorsten Werner <9943685+SamGamdschie@users.noreply.github.com>
2022-09-06 08:57:33 +0200

    Update to FreeBSD 13.1

--- jail_install.sh
+++ jail_install.sh
@@ -26,49 +26,49 @@ bastille bootstrap https://github.com/SamGamdschie/bastille-phpmyadmin
 bastille bootstrap https://github.com/SamGamdschie/bastille-php
 bastille bootstrap https://github.com/SamGamdschie/bastille-nextcloud
 bastille bootstrap https://github.com/SamGamdschie/bastille-wordpress
-bastille bootstrap https://github.com/SamGamdschie/bastille-wordpress
+#bastille bootstrap https://github.com/SamGamdschie/bastille-wordpress
 
 ## now create jails
-bastille create db 13.0-RELEASE 10.0.0.1
+bastille create db 13.1-RELEASE 10.0.0.1
 bastille template db SamGamdschie/bastille-mariadb
 
-bastille create letsencrypt 13.0-RELEASE 10.0.0.2
+bastille create letsencrypt 13.1-RELEASE 10.0.0.2
 bastille template letsencrypt SamGamdschie/bastille-letsencrypt
 
-bastille create mail 13.0-RELEASE 10.0.0.3
+bastille create mail 13.1-RELEASE 10.0.0.3
 bastille template mail SamGamdschie/bastille-mail
 
-bastille create proxy 13.0-RELEASE 10.0.0.4
+bastille create proxy 13.1-RELEASE 10.0.0.4
 bastille template proxy SamGamdschie/bastille-proxy
 
-bastille create postfixadmin 13.0-RELEASE 10.0.0.10
+bastille create postfixadmin 13.1-RELEASE 10.0.0.10
 bastille template postfixadmin SamGamdschie/bastille-postfixadmin
 
-bastille create phpmyadmin 13.0-RELEASE 10.0.0.11
+bastille create phpmyadmin 13.1-RELEASE 10.0.0.11
 bastille template phpmyadmin SamGamdschie/bastille-phpmyadmin
 
-bastille create matomo 13.0-RELEASE 10.0.0.12
+bastille create matomo 13.1-RELEASE 10.0.0.12
 bastille template matomo SamGamdschie/bastille-php
 
-bastille create cloud 13.0-RELEASE 10.0.0.20
+bastille create cloud 13.1-RELEASE 10.0.0.20
 bastille template cloud SamGamdschie/bastille-nextcloud
 
-bastille create heimen 13.0-RELEASE 10.0.0.21
+bastille create heimen 13.1-RELEASE 10.0.0.21
 bastille template heimen SamGamdschie/bastille-wordpress
 
-bastille create hobbingen 13.0-RELEASE 10.0.0.22
+bastille create hobbingen 13.1-RELEASE 10.0.0.22
 bastille template hobbingen SamGamdschie/bastille-wordpress
 
-bastille create seeadler 13.0-RELEASE 10.0.0.23
+bastille create seeadler 13.1-RELEASE 10.0.0.23
 bastille template seeadler SamGamdschie/bastille-wordpress
 
-bastille create mejep 13.0-RELEASE 10.0.0.24
+bastille create mejep 13.1-RELEASE 10.0.0.24
 bastille template mejep SamGamdschie/bastille-php
 
-bastille create werzel 13.0-RELEASE 10.0.0.25
+bastille create werzel 13.1-RELEASE 10.0.0.25
 bastille template werzel SamGamdschie/bastille-php
 
-bastille create thorsten 13.0-RELEASE 10.0.0.26
+bastille create thorsten 13.1-RELEASE 10.0.0.26
 bastille template thorsten SamGamdschie/bastille-php
 
 bastille cp ALL /werzerl/server_config/hosts.bastille etc/hosts
