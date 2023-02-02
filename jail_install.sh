14c8686a526182118fc5191cf4e559e02f0e849b
Thorsten Werner <9943685+SamGamdschie@users.noreply.github.com>
2023-02-02 10:20:31 +0100

    Minor Changes

--- jail_install.sh
+++ jail_install.sh
@@ -42,13 +42,18 @@ bastille bootstrap https://github.com/SamGamdschie/bastille-wordpress
 bastille create db 13.1-RELEASE 10.0.0.1
 bastille template db SamGamdschie/bastille-mariadb
 
-bastille create letsencrypt 13.1-RELEASE 10.0.0.2
-bastille template letsencrypt SamGamdschie/bastille-letsencrypt
+bastille create acme 13.1-RELEASE 10.0.0.2
+bastille template acme SamGamdschie/bastille-letsencrypt
 
 # Create first Certificates
 openssl dhparam -out /werzel/certificates/mail.werzelserver.de.512.pem 512
 openssl dhparam -out /werzel/certificates/mail.werzelserver.de.1024.pem 2048
 openssl dhparam -out /werzel/certificates/mail.werzelserver.de.dhparam.pem 4096
+bastille cmd acme acme.sh --home /var/db/acme/.acme.sh/ --register-account  -m acme@werzelserver.de --server letsencrypt+
+bastille cmd acme acme.sh --home /var/db/acme/.acme.sh/ --set-default-ca --server letsencrypt
+bastille cmd acme acme.sh --home /var/db/acme/.acme.sh/ --set-default-chain  --preferred-chain "ISRG"  --server letsencrypt
+bastille cmd acme acme.sh --home /var/db/acme/.acme.sh/ --dns dns_inwx --issue -d 'werzelserver.de' -d '*.werzelserver.de' --server letsencrypt
+bastille cmd acme acme.sh --home /var/db/acme/.acme.sh/ --dns dns_inwx --issue -d 'werzelserver.de' -d '*.werzelserver.de' --keylength ec-256 --server letsencrypt
 # letsencrypt!!
 # Also try ECC!
 
