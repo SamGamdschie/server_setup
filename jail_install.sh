35ab6c5c3d4418bdca284fcd7589bf03250b6165
Thorsten Werner <9943685+SamGamdschie@users.noreply.github.com>
2023-05-16 22:08:52 +0200

    Use sendmail as forwarder on host

--- jail_install.sh
+++ jail_install.sh
@@ -104,3 +104,16 @@ bastille template seeadler SamGamdschie/bastille-wordpress --arg config=seeadler
 bastille template mejep SamGamdschie/bastille-php --arg config=mejep
 bastille template werzel SamGamdschie/bastille-php --arg config=werzel
 bastille template thorsten SamGamdschie/bastille-php --arg config=thorsten
+
+# Use sendmail on host just to forward to postfix
+cd /etc/mail && make
+cd /etc/mail && sed '/^FEATURE.*dnl/d' `hostname`.submit.mc >> submit.mc.new
+cd /etc/mail && echo 'FEATURE(`msp'"'"', `[10.0.0.10]'"'"')dnl' >> submit.mc.new
+cd /etc/mail && mv `hostname`.submit.mc `hostname`.submit.mc.old
+cd /etc/mail && mv submit.mc.new `hostname`.submit.mc
+cd /etc/mail && make install
+sysrc sendmail_enable=NO
+sysrc sendmail_msp_queue_enable=YES
+sysrc sendmail_outbound_enable=NO
+sysrc submit_enable=YES
+cd /etc/mail && make stop && make start
