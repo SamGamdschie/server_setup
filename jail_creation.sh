ff0357cfa2ea58b8b335d5ccb2dbb1e72980abc4
Thorsten Werner <9943685+SamGamdschie@users.noreply.github.com>
2023-11-23 14:02:33 +0100

    Create jail_creation.sh

---
+++ jail_creation.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+
+# Install Bastille
+
+portmaster --packages-build --delete-build-only --no-confirm sysutils/bastille
+
+# Activate Networking
+sysrc cloned_interfaces+=lo1
+sysrc ifconfig_lo1_name="bastille0"
+service netif cloneup
+
+#Use ZFS
+sysrc -f /usr/local/etc/bastille/bastille.conf bastille_zfs_enable=YES
+sysrc -f /usr/local/etc/bastille/bastille.conf bastille_zfs_zpool=zroot
+sysrc -f /usr/local/etc/bastille/bastille.conf bastille_zfs_prefix="werzel/bastille"
+
+# Correct permissions
+chmod 0750 /usr/local/bastille
+
+### Certbot subdirs
+mkdir -p /werzel/certificates/live
+mkdir -p /werzel/certificates/archive
+
+# Create jails from templates
+## first bootstrap everything
+# BASE
+bastille bootstrap 13.2-RELEASE update
+bastille bootstrap https://github.com/SamGamdschie/bastille-mariadb
+bastille bootstrap https://github.com/SamGamdschie/bastille-letsencrypt
+# MAIL
+bastille bootstrap https://github.com/SamGamdschie/bastille-clamav
+bastille bootstrap https://github.com/SamGamdschie/bastille-solr
+bastille bootstrap https://github.com/SamGamdschie/bastille-redis
+bastille bootstrap https://github.com/SamGamdschie/bastille-mail
+#WEB
+bastille bootstrap https://github.com/SamGamdschie/bastille-proxy
+bastille bootstrap https://github.com/SamGamdschie/bastille-postfixadmin
+bastille bootstrap https://github.com/SamGamdschie/bastille-phpmyadmin
+bastille bootstrap https://github.com/SamGamdschie/bastille-nextcloud
+bastille bootstrap https://github.com/SamGamdschie/bastille-php
+bastille bootstrap https://github.com/SamGamdschie/bastille-wordpress
+#bastille bootstrap https://github.com/SamGamdschie/bastille-wordpress
+
+## now create all jails
+bastille create db 13.2-RELEASE 10.0.0.1
+bastille create certbot 13.2-RELEASE 10.0.0.2
+bastille create mail 13.2-RELEASE 10.0.0.10
+bastille create redis 13.2-RELEASE 10.0.0.11
+bastille create solr 13.2-RELEASE 10.0.0.12
+bastille create clamav 13.2-RELEASE 10.0.0.13
+bastille create proxy 13.2-RELEASE 10.0.0.20
+bastille create postfixadmin 13.2-RELEASE 10.0.0.21
+bastille create phpmyadmin 13.2-RELEASE 10.0.0.22
+bastille create matomo 13.2-RELEASE 10.0.0.23
+bastille create cloud 13.2-RELEASE 10.0.0.30
+bastille create heimen 13.-RELEASE 10.0.0.31
+bastille create hobbingen 13.2-RELEASE 10.0.0.32
+bastille create seeadler 13.2-RELEASE 10.0.0.33
+bastille create mejep 13.2-RELEASE 10.0.0.34
+bastille create werzel 13.2-RELEASE 10.0.0.35
+bastille create thorsten 13.2-RELEASE 10.0.0.36
+bastille create autoconfig 13.2-RELEASE 10.0.0.37
+
+## Add new jails to all host files
+bastille cp ALL /werzel/server_config/hosts.bastille etc/hosts
+cat /werzel/server_config/hosts.bastille >> /etc/hosts
